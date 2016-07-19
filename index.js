b9b9a15cb45e9d95986f74e91444ab990bc4f76a
leearaneta <lee.araneta@gmail.com>
2016-07-19 18:35:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kitten) {
+  var new_array = kittens.slice()
+  new_array.push(kitten);
+  return new_array;
+}
+
+function prependKitten(kitten) {
+  var new_array = kittens.slice()
+  new_array.unshift(kitten);
+  return new_array;
+}
+
+function removeLastKitten() {
+  var new_array = kittens.slice();
+  new_array.pop();
+  return new_array;
+}
+
+function removeFirstKitten() {
+  var new_array = kittens.slice();
+  new_array.shift();
+  return new_array;
+}
